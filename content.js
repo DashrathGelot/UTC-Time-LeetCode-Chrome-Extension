ab876a44a25e8cc1f8760512419cf42981eb824f
Dashrath Gelot <dash@Dashraths-Air.cable.rcn.com>
2023-03-26 14:55:12 -0500

    Added

--- content.js
+++ content.js
@@ -1,5 +1,37 @@
-let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+let days = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+let start = Date.now();
+const timer = () => {
+    let delta = Math.floor((Date.now() - start)/1000);
+    let min = Math.floor(delta/60);
+    let sec = delta%60;
+    if (min === 30) {
+        alert("Times UP....");
+    }
+    let mainContent = document.querySelector('[href="/subscribe/?ref=lp_pl&source=nav-premium"]');
+    var content = document.createElement("div");
+    content.id = "timer";
+    content.style.alignSelf = "center";
+    content.style.marginRight = "5px";
+    content.style.backgroundColor = "#073e52";
+    content.style.padding = "3px";
+    content.style.borderRadius = "2px";
+    if (min > 25) {
+        content.style.backgroundColor = "#e81607";
+    }
+    var paragraph = document.createElement("p");
+    let m = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+    paragraph.textContent = m + " : " + sec;
+    content.appendChild(paragraph);
+    let parent = mainContent.parentNode;
+    let old = document.getElementById("timer");
+    if (parent.contains(old)) {
+        parent.removeChild(old);
+    }
+    parent.prepend(content);
+}
 
 const timeStr = () => {
     let mainContent = document.querySelector('[href="/subscribe/?ref=lp_pl&source=nav-premium"]');
@@ -15,7 +47,7 @@ const timeStr = () => {
     var min = m < 10 ? '0' + m : m;
     var ampm = h < 12 ? 'AM' : 'PM';
     var time = hour + ':' + min + ' ' + ampm;
-    paragraph.textContent = days[d.getUTCDay() - 1]+", " + d.getUTCDate() + " " + months[d.getUTCMonth()] + " " + d.getUTCFullYear() + " " + time;
+    paragraph.textContent = days[d.getUTCDay()]+", " + d.getUTCDate() + " " + months[d.getUTCMonth()] + " " + d.getUTCFullYear() + " " + time;
     content.appendChild(paragraph);
     let parent = mainContent.parentNode;
     let old = document.getElementById("time");
@@ -28,4 +60,5 @@ const timeStr = () => {
 setTimeout(() => {
     timeStr();
     setInterval(timeStr, 60000);
-}, 1000);
+    setInterval(timer, 1000);
+}, 2000);
